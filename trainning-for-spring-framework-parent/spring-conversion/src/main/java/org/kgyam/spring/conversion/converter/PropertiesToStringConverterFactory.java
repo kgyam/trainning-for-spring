aa1a90d4cb397599e4738d69ffdf1c5795308078
kgyam <kgyam0122@163.com>
2021-04-09 13:47:22 +0800

    add conversion

--- trainning-for-spring-framework-parent/spring-conversion/src/main/java/org/kgyam/spring/conversion/converter/PropertiesToStringConverterFactory.java
+++ trainning-for-spring-framework-parent/spring-conversion/src/main/java/org/kgyam/spring/conversion/converter/PropertiesToStringConverterFactory.java
@@ -3,6 +3,7 @@ package org.kgyam.spring.conversion.converter;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.convert.converter.ConverterFactory;
 
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -13,6 +14,21 @@ import java.util.Properties;
 public class PropertiesToStringConverterFactory implements ConverterFactory<Properties, String> {
     @Override
     public <T extends String> Converter<Properties, T> getConverter(Class<T> aClass) {
-        return null;
+
+        return new InnerPropertiesToStringConverter ();
+    }
+
+
+    private static final class InnerPropertiesToStringConverter<T extends String> implements Converter<Properties, String> {
+
+
+        @Override
+        public String convert(Properties properties) {
+            StringBuilder stringBuilder = new StringBuilder ();
+            for (Map.Entry<Object, Object> entry : properties.entrySet ()) {
+                stringBuilder.append (entry.getKey () + ":" + entry.getValue ()).append (",");
+            }
+            return stringBuilder.deleteCharAt (stringBuilder.length () - 1).toString ();
+        }
     }
 }
